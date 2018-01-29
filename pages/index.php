2eba94271c8391292d10dfc92975051f40c27c53
Dustin Spitz <dts5425@rit.edu>
2018-01-29 17:07:25 -0500

    Removed getting specific object

--- pages/index.php
+++ pages/index.php
@@ -4,33 +4,14 @@
 include '../services/ConnectDb.class.php';
 
  try{
-
- echo "beginning <br/>";
  $instance = ConnectDb::getInstance();
  $conn = $instance->getConnection();
  var_dump($conn);
- echo "<br/> end";
-
  }
  catch(Exception $e){
  	echo $e->getMessage();
  }
 
-echo "<br/> hi";
-$data = $instance->getAllAccounts();
-
-echo "<br/> hi2 <br/>";
-
-var_dump($data);
-echo "<br/>";
-echo "hi: " . $data[0]['firstName'];
-
-
-foreach($data as $acct){
-    echo "made it";
-    $fname = $acct->firstName;
-    echo $fname;
-}
 ?>
 
 <html>
