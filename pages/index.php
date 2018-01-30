7ac7c129f0ea955cfc5eaeba95896d5e9e95af8f
dustinspitz <dts5425@rit.edu>
2018-01-30 02:24:14 -0500

    Pull Account from DB

--- pages/index.php
+++ pages/index.php
@@ -1,27 +1,42 @@
 
 <?php
-
-include '../services/ConnectDb.class.php';
-
- try{
- $instance = ConnectDb::getInstance();
- $conn = $instance->getConnection();
- var_dump($conn);
- }
- catch(Exception $e){
- 	echo $e->getMessage();
- }
-
+    include '../services/ConnectDb.class.php';
 ?>
 
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
 
- <body>
+<h3>Test Connection</h3>
 
- <?php echo '<p>Hello World</p>'; ?> 
+<?php
+    try{
+        $instance = ConnectDb::getInstance();
+        $conn = $instance->getConnection();
+        var_dump($conn);
+    }
+    catch(Exception $e){
+        echo $e->getMessage();
+    }
+?>
 
- </body>
+<hr>
+
+<br/>
+
+<h3> Get Accounts </h3>
+<?php
+    $data = $instance->getAllAccounts();
+    var_dump($data);
+    echo "<br/>";
+    echo "hi: " . $data[0]['firstName'];
+
+?>
+
+<hr>
+<br/>
+
+</body>
 </html>
