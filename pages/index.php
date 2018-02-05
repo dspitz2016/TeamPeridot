193ade691dcef9c7213de17e6aaf246ab0a13478
dustinspitz <dts5425@rit.edu>
2018-02-05 00:27:40 -0500

    Styling Update

--- pages/index.php
+++ pages/index.php
@@ -6,8 +6,14 @@
     $main->getHeader();
     $main->getNavigationBar();
 
-    echo '<div id="map"></div>';
+    echo '<div class="container">';
+        echo '<div class="row">';
+            echo '<div class="col s3"><h2>Upcoming Events</h2></div>';
+            echo '<div class="col s9"><div id="map"></div></div>';
+        echo '</div>';
+    echo '</div>';
 
-    $main->getScripts();
+
+$main->getScripts();
     $main->getFooter();
 ?>
