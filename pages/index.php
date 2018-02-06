a102cd431bfc4dfd353f5305bc406109b53b592b
dustinspitz <dts5425@rit.edu>
2018-02-06 14:30:02 -0500

    Moved Complete Map to PHP

--- pages/index.php
+++ pages/index.php
@@ -29,62 +29,9 @@
 
 <script type="text/javascript">
 
-    var map;
-    function initMap() {
-
         <?php echo $mapService->initMap(); ?>
-        // map = new google.maps.Map(document.getElementById('map'), {
-        //     center: {lat: 43.1293659, lng: -77.6394728},
-        //     zoom: 25,
-        //     mapTypeId: 'satellite',
-        //     mapTypeControl: false,
-        //     streetViewControl: false
-        // });
-
-        // map.setTilt(45);
-        // var infoWindow = new google.maps.InfoWindow;
-
-        // var image = path/to/image;
-        // var marker = new google.maps.Marker({
-        //     position: {lat: 43.1293659, lng: -77.6394728},
-        //     map: map,
-        //     animation: google.maps.Animation.DROP,
-        //     title: "Hello World!"
-        //     //icon: image
-        // });
-
-
-        <?php echo $markers; ?>
-
-        // Try HTML5 geolocation.
-    //     if (navigator.geolocation) {
-    //         navigator.geolocation.getCurrentPosition(function(position) {
-    //             var pos = {
-    //                 lat: position.coords.latitude,
-    //                 lng: position.coords.longitude
-    //             };
-    //
-    //             infoWindow.setPosition(pos);
-    //             infoWindow.setContent('Location found.');
-    //             infoWindow.open(map);
-    //             map.setCenter(pos);
-    //             console.log(pos);
-    //         }, function() {
-    //             handleLocationError(true, infoWindow, map.getCenter());
-    //         });
-    //     } else {
-    //         // Browser doesn't support Geolocation
-    //         handleLocationError(false, infoWindow, map.getCenter());
-    //     }
-    // }
-
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-            'Error: The Geolocation service failed.' :
-            'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-    }
+        
+        <?php //echo $markers; ?>
 
 </script>
 
