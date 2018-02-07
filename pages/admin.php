fa873e1699750348bd0ab4353f8ca0d1dfb68c3c
dustinspitz <dts5425@rit.edu>
2018-02-06 19:58:05 -0500

    Modified admin.php landing page

--- pages/admin.php
+++ pages/admin.php
@@ -1,9 +1,13 @@
 <?php
 
-include '../../components/Main.class.php';
+include '../components/Main.class.php';
 
 $main = Main::getInstance();
+?>
 
+<h1> Admin Home Page </h1>
+
+<?php
 $main->getHeader("admin");
 $main->getNavigationBar();
 $main->getScripts();
