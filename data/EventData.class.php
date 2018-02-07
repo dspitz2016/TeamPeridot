2bc6bf873e15d15395947fa619289df4d319ebd0
dustinspitz <dts5425@rit.edu>
2018-02-07 15:47:53 -0500

    Event ConnectDB change

--- data/EventData.class.php
+++ data/EventData.class.php
@@ -5,7 +5,7 @@ include '../services/ConnectDb.class.php';
 class EventData {
 
     function getAllEventsOrderedByDate(){
-        return ConnectDb::getInstance()->returnObject("Event", "Select name, description, startTime, endTime, imagePath, imageDescription From Event order by startTime desc;");
+        return ConnectDb::getInstance()->returnObject("", "Select name, description, startTime, endTime, imagePath, imageDescription From Event order by startTime desc;");
     }
 }
 
