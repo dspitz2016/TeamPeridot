aeeac01eef9543cb0a79d06a5c13548caa65a4ba
dspitz2016 <dts5425@rit.edu>
2018-02-13 21:13:09 -0500

    Local Connection Mod

--- services/MapService.class.php
+++ services/MapService.class.php
@@ -1,5 +1,8 @@
 <?php
 
+ini_set( 'error_reporting', E_ALL );
+ini_set( 'display_errors', true );
+
 include '../data/MapData.class.php';
 include '../models/MapPin.class.php';
 
@@ -14,48 +17,6 @@ class MapService {
      * Initialize Map Object
      */
     public function initMap($pinArray) {
-//        $mapInit = "
-//                 map = new google.maps.Map(document.getElementById('map'), {
-//                    center: {lat: 43.1293659, lng: -77.6394728},
-//                    zoom: 25,
-//                    mapTypeId: 'satellite',
-//                    mapTypeControl: false,
-//                    streetViewControl: false
-//                });
-//
-//                map.setTilt(45);
-//
-//                ".$this->createMapPins($pinArray)."
-//                var infoWindow = new google.maps.InfoWindow;
-//
-//                if (navigator.geolocation) {
-//                    navigator.geolocation.getCurrentPosition(function(position) {
-//                        var pos = {
-//                            lat: position.coords.latitude,
-//                            lng: position.coords.longitude
-//                        };
-//
-//                        infoWindow.setPosition(pos);
-//                        infoWindow.setContent('Location found.');
-//                        infoWindow.open(map);
-//                        map.setCenter(pos);
-//                        console.log(pos);
-//                    }, function() {
-//                        handleLocationError(true, infoWindow, map.getCenter());
-//                    });
-//                } else {
-//                    // Browser doesn't support Geolocation
-//                    handleLocationError(false, infoWindow, map.getCenter());
-//                }
-//
-//            function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-//                infoWindow.setPosition(pos);
-//                infoWindow.setContent(browserHasGeolocation ?
-//                    'Error: The Geolocation service failed.' :
-//                    'Error: Your browser doesn\'t support geolocation.');
-//                infoWindow.open(map);
-//            }
-//        ";
 
         $mapInit = "
             var myLatlng = new google.maps.LatLng(43.129467, -77.639153);
@@ -105,7 +66,7 @@ class MapService {
         $mapData = new MapData();
         $pinData = $mapData->getAllTrackableObjectPinData();
         $allMapPins = array();
-
+        
         foreach($pinData as $pinArray){
             $pin = new MapPin(
                 $pinArray['idTrackableObject'],
