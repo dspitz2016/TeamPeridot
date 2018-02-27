9c04d8a2a57cf964e381101f954c51736a0993d7
dspitz2016 <dts5425@rit.edu>
2018-02-27 02:04:40 -0500

    Map panning fix 2

--- services/MapService.class.php
+++ services/MapService.class.php
@@ -43,25 +43,25 @@ class MapService {
 
         ".$this->createMapPins($mapPinObjects, $isLocation)."
 //                 // Try HTML5 geolocation.
-//                if (navigator.geolocation) {
-//                  navigator.geolocation.getCurrentPosition(function(position) {
-//                    var pos = {
-//                      lat: position.coords.latitude,
-//                      lng: position.coords.longitude
-//                    };
-//        
-//                    infoWindow.setPosition(pos);
-//                    infoWindow.setContent('Location found.');
-//                    infoWindow.open(map);
-//                    //map.setCenter(pos);
-//                  }, function() {
-//                    handleLocationError(true, infoWindow, map.getCenter());
-//                  });
-//                } else {
-//                  // Browser doesn't support Geolocation
-//                  handleLocationError(false, infoWindow, map.getCenter());
-//                }
-//              }
+                if (navigator.geolocation) {
+                  navigator.geolocation.getCurrentPosition(function(position) {
+                    var pos = {
+                      lat: position.coords.latitude,
+                      lng: position.coords.longitude
+                    };
+        
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent('Location found.');
+                    //infoWindow.open(map);
+                    //map.setCenter(pos);
+                  }, function() {
+                    //handleLocationError(true, infoWindow, map.getCenter());
+                  });
+                } else {
+                  // Browser doesn't support Geolocation
+                  //handleLocationError(false, infoWindow, map.getCenter());
+                }
+              }
         
               function handleLocationError(browserHasGeolocation, infoWindow, pos) {
                 infoWindow.setPosition(pos);
