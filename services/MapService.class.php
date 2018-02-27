00a9ec6168fb0ad48e52d1db2515026e45203627
dspitz2016 <dts5425@rit.edu>
2018-02-27 02:03:09 -0500

    Stop map panning to user location

--- services/MapService.class.php
+++ services/MapService.class.php
@@ -42,26 +42,26 @@ class MapService {
             infoWindow = new google.maps.InfoWindow;
 
         ".$this->createMapPins($mapPinObjects, $isLocation)."
-                 // Try HTML5 geolocation.
-                if (navigator.geolocation) {
-                  navigator.geolocation.getCurrentPosition(function(position) {
-                    var pos = {
-                      lat: position.coords.latitude,
-                      lng: position.coords.longitude
-                    };
-        
-                    infoWindow.setPosition(pos);
-                    infoWindow.setContent('Location found.');
-                    infoWindow.open(map);
-                    //map.setCenter(pos);
-                  }, function() {
-                    handleLocationError(true, infoWindow, map.getCenter());
-                  });
-                } else {
-                  // Browser doesn't support Geolocation
-                  handleLocationError(false, infoWindow, map.getCenter());
-                }
-              }
+//                 // Try HTML5 geolocation.
+//                if (navigator.geolocation) {
+//                  navigator.geolocation.getCurrentPosition(function(position) {
+//                    var pos = {
+//                      lat: position.coords.latitude,
+//                      lng: position.coords.longitude
+//                    };
+//        
+//                    infoWindow.setPosition(pos);
+//                    infoWindow.setContent('Location found.');
+//                    infoWindow.open(map);
+//                    //map.setCenter(pos);
+//                  }, function() {
+//                    handleLocationError(true, infoWindow, map.getCenter());
+//                  });
+//                } else {
+//                  // Browser doesn't support Geolocation
+//                  handleLocationError(false, infoWindow, map.getCenter());
+//                }
+//              }
         
               function handleLocationError(browserHasGeolocation, infoWindow, pos) {
                 infoWindow.setPosition(pos);
