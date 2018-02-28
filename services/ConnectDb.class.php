9c03b564127aad2da2a02f01d6b736a968a9b845
dspitz2016 <dts5425@rit.edu>
2018-02-28 18:00:19 -0500

    Clean & Commented Connectdb Class

--- services/ConnectDb.class.php
+++ services/ConnectDb.class.php
@@ -1,97 +1,87 @@
 <?php
 
-class ConnectDb {
-  private static $instance = null;
-  private $conn;
+/**
+ * Class ConnectDb
+ * Author: Dustin Spitz
+ * Purpose: This class is responsible for establishing a connection to the database
+ */
 
-  private $host = 'localhost:3306';
-  private $user = 'root';
-  private $pass = 'Ch33zeB@llFestival!!';
-  private $db = 'RapidsCemetery';
+class ConnectDb
+{
 
-  private function __construct()
-  {
-    try{
-        $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db}",$this->user,$this->pass);
-    }
+    private static $instance = null;
+    private $conn;
 
-    catch(PDOException $e){
-        echo "Fail";
-        echo $e->getMessage();
-        die();
-    }
+    // Database Connection Information (Should be in a separate file)
+    private $host = 'localhost:3306';
+    private $user = 'root';
+    private $pass = 'Ch33zeB@llFestival!!';
+    private $db = 'RapidsCemetery';
 
-    $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-  }
-
-  public static function getInstance()
-  {
-    if(!self::$instance)
+    /**
+     * ConnectDb constructor.
+     * Establish a connection to the database using PDO
+     */
+    private function __construct()
     {
-      self::$instance = new ConnectDb();
+        try {
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db}", $this->user, $this->pass);
+        } catch (PDOException $e) {
+            echo "Fail";
+            echo $e->getMessage();
+            die();
+        }
+
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
-    return self::$instance;
-  }
 
-  public function getConnection()
-  {
-    return $this->conn;
-  }
+    /**
+     * @return ConnectDb|null
+     * Get Instance establishes a singleton design pattern
+     */
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new ConnectDb();
+        }
+        return self::$instance;
+    }
 
-    /*
-   * Takes a table name, and optional sql string
-   * Prepares and executes the statement
-   * @param $objName
-   * @param $sqlString
-   * @return: $results
-   */
-    function returnObject($objName, $sqlString=""){
-        try{
+    /**
+     * @return PDO
+     * Returns established database connection
+     */
+    public function getConnection()
+    {
+        return $this->conn;
+    }
+
+
+    /**
+     * @param $objName - Name of Object / Database Table
+     * @param string $sqlString - Complete sql select statement
+     * @return array - An associative array of objects pulled from the database
+     */
+    function returnObject($objName, $sqlString = "")
+    {
+        try {
             $results = array();
-            if($sqlString == "") {
-                $sqlString = "SELECT * FROM " .$objName;
+            if ($sqlString == "") {
+                $sqlString = "SELECT * FROM " . $objName;
             }
             $stmnt = $this->conn->prepare($sqlString);
             $stmnt->execute();
-            $stmnt->setFetchMode(PDO::FETCH_CLASS,$objName);
-            while($result = $stmnt->fetch()){ // or just fetchALl();
+            $stmnt->setFetchMode(PDO::FETCH_CLASS, $objName);
+            while ($result = $stmnt->fetch()) { // or just fetchALl();
                 $results[] = $result;
             }
             return $results;
-        }
-        catch(PDOException $e){
+        } catch (PDOException $e) {
             echo $e->getMessage();
             die();
         }
     }
 
-    function getAllAccounts(){
-        return $this->returnObject("Account", "");
-    }
-
-    function getAllEvents(){
-        return $this->returnObject("Event", "");
-    }
-
-    function getAllFAQs(){
-        return $this->returnObject("FAQ", "");
-    }
-
-    function getAllLocations(){
-        return $this->returnObject("Location", "");
-    }
-
-    function getAllTypeFilters(){
-        return $this->returnObject("TypeFilter", "");
-    }
-
-    function getAllTrackableObjects(){
-        return $this->returnObject("TrackableObject", "");
-    }
-
-    function getAllHistoricFilters(){
-        return $this->returnObject("HistoricFilter", "");
-    }
 }
 
 ?>
