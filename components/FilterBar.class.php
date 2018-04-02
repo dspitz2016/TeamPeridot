5c42575d3ca33d9f11a033b32705c1b736062e09
dspitz2016 <dts5425@rit.edu>
2018-04-01 22:29:20 -0400

    Remove tool tip from filter bars

--- components/FilterBar.class.php
+++ components/FilterBar.class.php
@@ -6,7 +6,7 @@ class FilterBar {
     public function getTypeFilterBar($typeFilterObjectArray){
             echo '<div class="filterRow  white-text center cust-color-slate">';
                 foreach($typeFilterObjectArray as $typeFilter){
-                    echo '<button class="btn waves-effect waves-light tooltipped" data-position="top" data-delay="50" data-tooltip="'.$typeFilter->getDescription().'" type="submit" name="action" style="background-color:#'.$typeFilter->getButtonColor().';" onclick="setTypeFilter('.$typeFilter->getIdType().')">'.$typeFilter->getTypeFilter();
+                    echo '<button class="btn waves-effect waves-light" type="submit" name="action" style="background-color:#'.$typeFilter->getButtonColor().';" onclick="setTypeFilter('.$typeFilter->getIdType().')">'.$typeFilter->getTypeFilter();
                          echo '<i class="material-icons right">filter_list</i>';
                     echo '</button>';
                 }
