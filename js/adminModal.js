f3d1e443ab5ba968d632ec36b71788af5b4c87ef
dspitz2016 <dts5425@rit.edu>
2018-04-03 01:56:51 -0400

    Add back location reloads to admin site

--- js/adminModal.js
+++ js/adminModal.js
@@ -67,7 +67,7 @@ function modalController(action, obj, objId){
                     contentType : 'application/x-www-form-urlencoded; charset=UTF-8',
                     success: function(){
                         console.log("created object");
-                        //location.reload();
+                        location.reload();
                     },
                     error: function(xhr, ajaxOptions, thrownError){
                         alert(xhr.status);
@@ -93,7 +93,7 @@ function modalController(action, obj, objId){
                     contentType : 'application/x-www-form-urlencoded; charset=UTF-8',
                     success: function(){
                         console.log("updated object");
-                        // location.reload();
+                        location.reload();
 
                     },
                     error: function(xhr, ajaxOptions, thrownError){
@@ -123,7 +123,7 @@ function modalController(action, obj, objId){
                             $('#deleteModal .modal-footer').html("<button class='btn waves-effect waves-light modal-close' href='#deleteModal' type='submit'> Cancel</button>");
                         }
 
-                        // location.reload();
+                        location.reload();
                     },
                     error: function(xhr, ajaxOptions, thrownError){
                         alert(xhr.status);
